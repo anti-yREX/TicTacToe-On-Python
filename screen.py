5e6be0262a7d0f3a0330de7ede86c9b458f88ce4
Rituraj Ingale <ingalerd@gmail.com>
2019-08-10 23:13:29 +0530

    sticks are done

--- screen.py
+++ screen.py
@@ -1,6 +1,23 @@
 from graphics import *
 win = GraphWin("GAME",500,500)
+#rect = Rectangle(Point(0,100),Point(500,500))
+x = 100
+y = 150
+line = Line(Point(x+100 , 164) , Point(x+100 , y+300))
+line.setWidth(5)
+line.draw(win)
 
+line = Line(Point(x+200 , 164) , Point(x+200 , y+300))
+line.setWidth(5)
+line.draw(win)
 
+line = Line(Point(x , y+100) , Point(x+300,y+100))
+line.setWidth(5)
+line.draw(win)
+
+line = Line(Point(x , y+200) , Point(x+300,y+200))
+line.setWidth(5)
+line.draw(win)
+#182,(164 + 100 + 100)
 win.getMouse()
 win.close()
