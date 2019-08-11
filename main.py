2961eaef41ba2ab73b75c2d2836f6b653edf8920
Rituraj Ingale <ingalerd@gmail.com>
2019-08-12 00:21:09 +0530

    logic is all messed up
    ay yo logic waddup!!!

--- main.py
+++ main.py
@@ -1,24 +1,66 @@
-from screen import Close_window
+from screen import Close_window , Get_Input_xy , Put_Output_xy , Show_Result
 from math import floor
 
-pt = Close_window()
-x = pt.getX()
-y = pt.getY()
-a = [-1 ,-1]
-if(100 < x < 400) & (150 < y < 450):
-    if x<200:
-        x = 1
-    elif x<300:
-        x = 2
+def process_xy(x,y):
+    if(100 < x < 400) & (150 < y < 450):
+        x = x - 100
+        x = floor(x/100)
+        y = y - 150
+        y = floor(y/100)
+        return x , y
     else:
-        x = 3
+        return -1,-1
+
+def check_game(a):
+    n = 0
+    for i in range(0,2):
+        if a[i][0] == a[i][1] == a[i][2] != -1:
+            print(str(i)+" a[i][0] == a[i][1] == a[i][2] != -1:")
+            n = 1
+            break
+        if a[0][i] == a[1][i] == a[2][i] != -1:
+            print(str(i)+" a[0][i] == a[1][i] == a[2][i] != -1:")
+            n = 1
+            break
+    if a[0][0] == a[1][1] == a[2][2] != -1:
+        print("Cross 1")
+        n = 1
+    if a[2][0] == a[1][1] == a[0][2] != -1:
+        print("Cross 2")
+        n = 1
     
-    if y<250:
-        y = 1
-    elif y<350:
-        y = 2
-    else:    
-        y = 3
+    if n==0:
+        n=2
+        for i in range(0,2):
+            for j in range(0,2):
+                if a[i][j]==-1:
+                    return 0
     
-else:
-    print("Invalid")
+    if n==2:
+        print("Draw")        
+    return n
+
+a = [[-1,-1,-1],
+    [-1,-1,-1],
+    [-1,-1,-1]]
+flag = 0
+p = 0
+while flag != 1:
+    pt = Get_Input_xy(p)
+    x,y = process_xy(int(pt.getX()),int(pt.getY()))
+    if (x != -1) & (y != -1):
+        if(a[y][x] == -1):
+            a[y][x] = p
+            Put_Output_xy(x,y,p)
+            chk = check_game(a)
+            if chk!=0:
+                Show_Result(p,chk)
+                flag = 1
+            p += 1
+            if(p > 1):
+                p = 0
+    
+print(a[0])
+print(a[1])
+print(a[2])
+Close_window()
--- screen.py
+++ screen.py
@@ -2,10 +2,10 @@ from graphics import *
 
 win = GraphWin("GAME",500,550)
 
-blue = "#0099ff"
+blue = "#5c9df7"
 blue_d = "#0000ff" 
 
-red = "#ff3000"
+red = "#f7665c"
 red_d = "#ff0000"
 
 x = 100
@@ -36,6 +36,57 @@ text.setStyle("bold")
 text.setText("X")
 text.draw(win)
 
+def Get_Input_xy(n):
+    if n==0:
+        c = red
+        stri = "X"
+    if n==1:
+        c = blue
+        stri = "O"
+    rect  = Rectangle(Point(75,45),Point(425,115))
+    rect.setFill(c)
+    rect.draw(win)
+    text = Text(Point(250,80),stri)
+    text.setSize(30)
+    text.setStyle("bold")
+    text.draw(win)
+    pt = win.getMouse()
+    return pt
+
+def Put_Output_xy(x,y,n):
+    x = (100*x)+150
+    y = (100*y)+200
+    if n == 0:
+        c = red_d
+        stri = "X"
+    if n == 1:
+        c = blue_d
+        stri = "O"
+    text = Text(Point(x,y),stri)
+    text.setSize(30)
+    text.setStyle("bold")
+    text.setTextColor(c)
+    text.draw(win)
+    return
+
+def Show_Result(n,chk):
+    stri = ""
+    if chk==1:
+        if n == 0:
+            stri = "X has won!!"
+        if n == 1:
+            stri = "O has won!!"
+    if chk==2:
+        stri == "its A Draw!!"
+    rect  = Rectangle(Point(75,45),Point(425,115))
+    rect.setFill("#525252")
+    rect.draw(win)
+    text = Text(Point(250,80),stri)
+    text.setSize(30)
+    text.setStyle("bold")
+    text.draw(win)
+    return
+    
 def Close_window():
     pt = win.getMouse()
     print(str(pt.getX)+"  "+str(pt.getY))
